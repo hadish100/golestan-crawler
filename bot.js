bebcfa51a830f7bbd512628a91c16de39eabef72
pouya <hadishhamraz@gmail.com>
2024-08-07 19:08:07 +0330

    added check_courses utility

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.telegram_bot_token);
-const admin_id = process.env.chat_id;
+const { grade_chat_id } = process.env;
+const { course_chat_id } = process.env;
 
 
 async function alert_new_grade(course_name,course_grade)
@@ -12,26 +13,26 @@ async function alert_new_grade(course_name,course_grade)
     msg += course_name
     msg += "</b>"
     msg += " در سامانه گلستان وارد شد."
-    await bot.telegram.sendMessage(admin_id, msg, {parse_mode: 'HTML'});
+    await bot.telegram.sendMessage(grade_chat_id, msg, {parse_mode: 'HTML'});
 
     var msg = "🔸 "
     msg += "نمره شما در این درس: "
     msg += "<b>"
     msg += course_grade
     msg += "</b>"
-    await bot.telegram.sendMessage(admin_id, msg, {parse_mode: 'HTML'});
+    await bot.telegram.sendMessage(grade_chat_id, msg, {parse_mode: 'HTML'});
 }
 
 async function alert_no_new_grade()
 {
     var msg = "💢 اکانت گلستان شما بررسی شد. نمره جدیدی در آن به ثبت نرسیده است.";
-    await bot.telegram.sendMessage(admin_id, msg);
+    await bot.telegram.sendMessage(grade_chat_id, msg);
 }
 
-async function init_msg(grade_report)
+async function init_grade_msg(grade_report)
 {
     var msg = "🔻اکانت گلستان شما بررسی شد، نمرات ثبت شده تا الان:";
-    await bot.telegram.sendMessage(admin_id, msg, {parse_mode: 'HTML'});
+    await bot.telegram.sendMessage(grade_chat_id, msg, {parse_mode: 'HTML'});
 
     for(var i=0;i<grade_report.length;i++)
     {
@@ -43,8 +44,104 @@ async function init_msg(grade_report)
         msg += "</b>";
         msg += " : ";
         msg += grade_report[i].course_grade;
-        await bot.telegram.sendMessage(admin_id, msg, {parse_mode: 'HTML'});
+        await bot.telegram.sendMessage(grade_chat_id, msg, {parse_mode: 'HTML'});
     }
 }
 
-module.exports = {alert_new_grade, alert_no_new_grade, init_msg};
+async function alert_new_courses(course_report)
+{
+    var start_msg = "🔹 دروس جدیدی در گلستان به ثبت رسیده است:";
+    await bot.telegram.sendMessage(course_chat_id, start_msg, {parse_mode: 'HTML'});
+
+    var msg = "";
+
+    for(var i=0;i<course_report.length;i++)
+    {
+        msg += "🔺 کد درس:";
+        msg += "<b>";
+        msg += " ";
+        msg += course_report[i].course_code;
+        msg += " ";
+        msg += "</b>";
+
+        msg += "\n";
+
+        msg += "🔸 نام درس:"
+        msg += "<b>";
+        msg += " ";
+        msg += course_report[i].course_name;
+        msg += " ";
+        msg += "</b>";
+
+        msg += "\n";
+
+        msg += "🔹 استاد درس:"
+        msg += "<b>";
+        msg += " ";
+        msg += course_report[i].course_prof;
+        msg += " ";
+        msg += "</b>";
+
+        msg += "\n";
+        msg += "\n";
+
+    }
+
+    await bot.telegram.sendMessage(course_chat_id, msg, {parse_mode: 'HTML'});
+}
+
+async function alert_no_new_course()
+{
+    var msg = "💢 گزارش ۱۰۲ گلستان بررسی شد. درس جدیدی در آن به ثبت نرسیده است.";
+    await bot.telegram.sendMessage(course_chat_id, msg);
+}
+
+async function init_course_msg(course_report)
+{
+    var start_msg = "🔻گزارش ۱۰۲ گلستان بررسی شد. دروس ثبت شده تا این لحظه:";
+    await bot.telegram.sendMessage(course_chat_id, start_msg, {parse_mode: 'HTML'});
+
+    var msg = "";
+
+    for(var i=0;i<course_report.length;i++)
+    {
+        msg += "🔺 کد درس:";
+        msg += "<b>";
+        msg += " ";
+        msg += course_report[i].course_code;
+        msg += " ";
+        msg += "</b>";
+
+        msg += "\n";
+
+        msg += "🔸 نام درس:"
+        msg += "<b>";
+        msg += " ";
+        msg += course_report[i].course_name;
+        msg += " ";
+        msg += "</b>";
+
+        msg += "\n";
+
+        msg += "🔹 استاد درس:"
+        msg += "<b>";
+        msg += " ";
+        msg += course_report[i].course_prof;
+        msg += " ";
+        msg += "</b>";
+
+        msg += "\n";
+        msg += "\n";
+
+    }
+
+    await bot.telegram.sendMessage(course_chat_id, msg, {parse_mode: 'HTML'});
+}
+
+async function test()
+{
+    await bot.telegram.sendMessage(grade_chat_id, "TEST");
+    await bot.telegram.sendMessage(course_chat_id, "TEST");
+}
+
+module.exports = {alert_new_grade, alert_no_new_grade, init_grade_msg, init_course_msg, alert_no_new_course, alert_new_courses, test};
--- crawler.js
+++ crawler.js
@@ -3,14 +3,13 @@ const puppeteer = require("puppeteer");
 const fs = require("fs").promises;
 const axios = require('axios');
 
-const {alert_new_grade,log_report} = require("./bot.js");
 const {solve_captcha,sleep,generate_random_name} = require("./captcha_solver.js");
 
 async function check_golestan_for_new_grades()
 {
     try
     {
-        var browser = await puppeteer.launch({headless:false});
+        var browser = await puppeteer.launch({headless:true});
         var page = await browser.newPage();
         page.setDefaultTimeout(30000);
     
@@ -150,5 +149,241 @@ async function check_golestan_for_new_grades()
 
 }
 
+async function check_golestan_for_new_courses()
+{
+    try
+    {
+        var browser = await puppeteer.launch({headless:true});
+        var page = await browser.newPage();
+        page.setDefaultTimeout(30000);
+    
+        await page.goto("https://golestan.sbu.ac.ir/forms/authenticateuser/main.htm");
+        await page.setViewport({width: 1800,height: 1000});
+    
+        await page.waitForSelector('iframe[name="Faci1"]');
+        var frameHandle1 = await page.$('iframe[name="Faci1"]');
+        var frame1 = await frameHandle1.contentFrame();
+        
+        await frame1.waitForSelector('frame[name="Master"]');
+        var frameHandle2 = await frame1.$('frame[name="Master"]');
+        var frame2 = await frameHandle2.contentFrame();
+        
+        await frame2.waitForSelector('frame[name="Form_Body"]');
+        var frameHandle3 = await frame2.$('frame[name="Form_Body"]');
+        var frame3 = await frameHandle3.contentFrame();
+        
+        await frame3.waitForSelector("#F80351", { visible: true });
+        await frame3.waitForSelector("#F80401", { visible: true });
+        
+        await frame3.waitForSelector("#imgCaptcha", { visible: true });
+        const captcha_image = await frame3.$("#imgCaptcha");
+        var path = "./captchas/" + generate_random_name() + ".gif";
+        const image_url = await captcha_image.evaluate(node => node.src);
+        var response = await axios.get(image_url, {responseType: 'arraybuffer'});
+        await fs.writeFile(path, response.data);
+    
+    
+        const {golestan_username,golestan_password} = process.env;
+        const captcha_text = await solve_captcha(path,false);
+    
+        console.log(captcha_text);
+    
+        await frame3.evaluate((golestan_username, golestan_password, captcha_text) => 
+        {
+            document.querySelector("#F80351").value = golestan_username;
+            document.querySelector("#F80401").value = golestan_password;
+            document.querySelector("#F51701").value = captcha_text;
+        }, golestan_username, golestan_password, captcha_text);
+    
+    
+        await frame3.evaluate(() => 
+        {
+            document.querySelectorAll("#btnLog")[0].click();
+        });
+    
+        await sleep(3000)
+    
+        await page.waitForSelector('iframe[name="Faci2"]');
+        var frameHandle4 = await page.$('iframe[name="Faci2"]');
+        var frame4 = await frameHandle4.contentFrame();
+        
+        var frameHandle5 = await frame4.$('frame[name="Master"]');
+        var frame5 = await frameHandle5.contentFrame();
+    
+        var frameHandle6 = await frame5.$('frame[name="Form_Body"]');
+        var frame6 = await frameHandle6.contentFrame();
+    
+        await frame6.waitForSelector(".menuright #mendiv > table > tbody tr:nth-child(6) td");
+        
+        await sleep(3000);
+    
+        await frame6.evaluate(() =>
+        {
+            document.querySelector("#F20851").value = "102";
+            dirok();
+        });
+    
+    
+        await page.waitForSelector('iframe[name="Faci3"]');
+        await sleep(3000);
+    
+        const courses_report = await page.evaluate(async () => 
+        {
+            const frame = document.querySelector('iframe[name="Faci3"]');
+            const frameDocument = frame.contentDocument || frame.contentWindow.document;
+    
+            const masterFrame = frameDocument.querySelector('frame[name="Master"]');
+            const masterFrameDocument = masterFrame.contentDocument || masterFrame.contentWindow.document;
+    
+            const formBodyFrame = masterFrameDocument.querySelector('frame[name="Form_Body"]');
+            const formBodyFrameDocument = formBodyFrame.contentDocument || formBodyFrame.contentWindow.document;
+    
+            var input_1 = formBodyFrameDocument.querySelectorAll("#SF411522_0")[0]; // #GF078012_0
+            input_1.value = "43";
+            input_1.dispatchEvent(new Event('change', { bubbles: true }));    
 
-module.exports = {check_golestan_for_new_grades};
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+
+            const commanderFrame = frameDocument.querySelector('frame[name="Commander"]');
+            const commanderFrameDocument = commanderFrame.contentDocument || commanderFrame.contentWindow.document;
+
+            commanderFrameDocument.querySelectorAll("#IM16_ViewRep")[0].click();
+
+            await new Promise(resolve => setTimeout(resolve, 5000));
+
+            const headerFrame = masterFrameDocument.querySelector('frame[name="Header"]');
+            const headerFrameDocument = headerFrame.contentDocument || headerFrame.contentWindow.document;
+
+            const formBodyFrame2 = headerFrameDocument.querySelector('frame#Form_Body');
+            const formBodyFrameDocument2 = formBodyFrame2.contentDocument || formBodyFrame2.contentWindow.document;
+
+            var courses = [];
+            
+            while(true)
+            {
+                var course_trs = formBodyFrameDocument2.querySelectorAll("table#Table3 tbody tr");
+
+                for(var i = 0; i < course_trs.length; i++)
+                {
+                    courses.push
+                    ({
+                        course_code:course_trs[i].querySelectorAll("td")[0].innerText,
+                        course_name:course_trs[i].querySelectorAll("td")[1].innerText.replaceAll("ي","ی").replaceAll("ك","ک"),
+                        course_prof:course_trs[i].querySelectorAll("td")[8].innerText.replaceAll("ي","ی").replaceAll("ك","ک").replace("\n",""),
+                        course_time:course_trs[i].querySelectorAll("td")[9].innerText.replaceAll("ي","ی").replaceAll("ك","ک"),
+                    })
+                }
+
+
+                const frame3 = document.querySelector('iframe[name="Faci3"]');
+                const frameDocument3 = frame3.contentDocument || frame3.contentWindow.document;
+
+                const commanderFrame3 = frameDocument3.querySelector('frame[name="Commander"]');
+                const commanderFrameDocument3 = commanderFrame3.contentDocument || commanderFrame3.contentWindow.document;
+
+                commanderFrameDocument3.querySelectorAll("#MoveLeft")[0].click();
+
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                if(courses[courses.length-1].course_code == course_trs[course_trs.length-1].querySelectorAll("td")[0].innerText) break;
+            }
+
+            const frame4 = document.querySelector('iframe[name="Faci3"]');
+            const frameDocument4 = frame4.contentDocument || frame4.contentWindow.document;
+
+            const commanderFrame4 = frameDocument4.querySelector('frame[name="Commander"]');
+            const commanderFrameDocument4 = commanderFrame4.contentDocument || commanderFrame4.contentWindow.document;
+
+            commanderFrameDocument4.querySelectorAll("#IM90_gomenu")[0].click();
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const faci2Frame = document.querySelector('iframe[name="Faci2"]');
+            const faci2FrameDocument = faci2Frame.contentDocument || faci2Frame.contentWindow.document;
+
+            const faci2MasterFrame = faci2FrameDocument.querySelector('frame[name="Master"]');
+            const faci2MasterFrameDocument = faci2MasterFrame.contentDocument || faci2MasterFrame.contentWindow.document;
+
+            const faci2FormBodyFrame = faci2MasterFrameDocument.querySelector('frame[name="Form_Body"]');
+            const faci2FormBodyFrameDocument = faci2FormBodyFrame.contentDocument || faci2FormBodyFrame.contentWindow.document;
+
+            faci2FormBodyFrameDocument.querySelectorAll("#OK")[0].click()
+
+            await new Promise(resolve => setTimeout(resolve, 5000));
+
+            const frame5 = document.querySelector('iframe[name="Faci4"]');
+            const frameDocument5 = frame5.contentDocument || frame5.contentWindow.document;
+
+            const masterFrame5 = frameDocument5.querySelector('frame[name="Master"]');
+            const masterFrameDocument5 = masterFrame5.contentDocument || masterFrame5.contentWindow.document;
+
+            const formBodyFrame5 = masterFrameDocument5.querySelector('frame[name="Form_Body"]');
+            const formBodyFrameDocument5 = formBodyFrame5.contentDocument || formBodyFrame5.contentWindow.document;
+
+            var input_2 = formBodyFrameDocument5.querySelectorAll("#GF078012_0")[0];
+            input_2.value = "43";
+            input_2.dispatchEvent(new Event('change', { bubbles: true }));
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const commanderFrame5 = frameDocument5.querySelector('frame[name="Commander"]');
+            const commanderFrameDocument5 = commanderFrame5.contentDocument || commanderFrame5.contentWindow.document;
+
+            commanderFrameDocument5.querySelectorAll("#IM16_ViewRep")[0].click();
+
+            await new Promise(resolve => setTimeout(resolve, 5000));
+
+            const headerFrame5 = masterFrameDocument5.querySelector('frame[name="Header"]');
+            const headerFrameDocument5 = headerFrame5.contentDocument || headerFrame5.contentWindow.document;
+
+            const formBodyFrame6 = headerFrameDocument5.querySelector('frame#Form_Body');
+            const formBodyFrameDocument6 = formBodyFrame6.contentDocument || formBodyFrame6.contentWindow.document;
+
+            while(true)
+            {
+                var course_trs = formBodyFrameDocument6.querySelectorAll("table#Table3 tbody tr");
+
+                for(var i = 0; i < course_trs.length; i++)
+                {
+                    courses.push
+                    ({
+                        course_code:course_trs[i].querySelectorAll("td")[0].innerText,
+                        course_name:course_trs[i].querySelectorAll("td")[1].innerText.replaceAll("ي","ی").replaceAll("ك","ک"),
+                        course_prof:course_trs[i].querySelectorAll("td")[8].innerText.replaceAll("ي","ی").replaceAll("ك","ک").replace("\n",""),
+                        course_time:course_trs[i].querySelectorAll("td")[9].innerText.replaceAll("ي","ی").replaceAll("ك","ک"),
+                    })
+                }
+
+                const frame3 = document.querySelector('iframe[name="Faci3"]');
+                const frameDocument3 = frame3.contentDocument || frame3.contentWindow.document;
+
+                const commanderFrame3 = frameDocument3.querySelector('frame[name="Commander"]');
+                const commanderFrameDocument3 = commanderFrame3.contentDocument || commanderFrame3.contentWindow.document;
+
+                commanderFrameDocument3.querySelectorAll("#MoveLeft")[0].click();
+
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                if(courses[courses.length-1].course_code == course_trs[course_trs.length-1].querySelectorAll("td")[0].innerText) break;
+
+            }
+
+            return courses
+
+        });
+
+
+        await page.close();
+        await browser.close();
+        return courses_report;
+    }
+
+    catch(e)
+    {
+        await page.close();
+        await browser.close();
+        throw e;
+    }
+
+}
+
+
+module.exports = {check_golestan_for_new_grades,check_golestan_for_new_courses};
